6c1624ef26269f3610b6f86f14e30d51fe2e2cbd
berkagmpp <berkagmpp@gmail.com>
2022-05-22 10:46:24 +1200

    add Dummy-data and list up in <ul> in Products.js / get props data from Products.js and dispatch to addToCartHandler in ProductItem.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,48 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+    {
+        id: 'p1',
+        title: 'Atonement',
+        writer: 'Ian McEwan',
+        price: 6,
+        description: 'It tells the story of protagonist Briony Tallis crime and how it changes her life, as well as those of her sister Cecilia and her lover Rob.'
+    },
+    {
+        id: 'p2',
+        title: 'The Corrections',
+        writer: 'Jonathan Franzen',
+        price: 5,
+        description: 'The Corrections is a 2001 novel by American author Jonathan Franzen.'
+    },
+    {
+        id: 'p3',
+        title: 'The Road',
+        writer: 'Cormac McCarthy',
+        price: 6.5,
+        description: 'The book details the grueling journey of a father and his young son over a period of several months across a landscape blasted by an unspecified cataclysm that has destroyed industrial civilization and almost all life.'
+    }
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite books</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(book => (
+                    <ProductItem
+                        key={book.id}
+                        id={book.id}
+                        title={book.title}
+                        writer={book.writer}
+                        price={book.price}
+                        description={book.description}
+                    />
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
