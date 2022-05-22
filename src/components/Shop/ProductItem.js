bcc87e006007cd13ff4639cede5aa6e146a1f833
berkagmpp <berkagmpp@gmail.com>
2022-05-22 15:45:10 +1200

    revise updatedItem to send backend inside the components in ProductItem.js

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,6 +1,6 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { cartActions } from '../../store/cart-slice';
+import { cartActions  } from '../../store/cart-slice';
 import Card from '../UI/Card';
 
 import classes from './ProductItem.module.css';
@@ -8,14 +8,46 @@ import classes from './ProductItem.module.css';
 const ProductItem = (props) => {
     const dispatch = useDispatch();
 
+    const cart = useSelector(state => state.cart);
+
     const { id, title, writer, price, description } = props;
 
     const addToCartHandler = () => {
-        dispatch(cartActions.addItemToCart({
-            id,
-            title,
-            price
-        }));
+        const newTotalQuantity = cart.totalQuantity + 1;
+
+        const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+        const existingItem = updatedItems.find((item) => item.id === id);
+        
+        if (existingItem) {
+          const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+          updatedItem.quantity++;
+          updatedItem.totalPrice = updatedItem.totalPrice + price;
+          const existingItemIndex = updatedItems.findIndex(
+            (item) => item.id === id
+          );
+          updatedItems[existingItemIndex] = updatedItem;
+        } else {
+          updatedItems.push({
+            id: id,
+            price: price,
+            quantity: 1,
+            totalPrice: price,
+            name: title,
+          });
+        }
+    
+        const newCart = {
+          totalQuantity: newTotalQuantity,
+          items: updatedItems,
+        };
+    
+        dispatch(cartActions.replaceCart(newCart));
+
+        // dispatch(cartActions.addItemToCart({
+        //     id,
+        //     title,
+        //     price
+        // }));
     };
 
     return (
