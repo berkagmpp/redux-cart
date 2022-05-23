7cc028d8a13980b96f3490c874cf2ae84efc366e
berkagmpp <berkagmpp@gmail.com>
2022-05-23 14:33:59 +1200

    async/await HTTP request, response.json in App.js / add showNotification action for HTTP status in ui-slice.js

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ function App() {
     const cart = useSelector(state => state.cart);
 
     useEffect(() => {
-        fetch(
-            'https://react-book-cart-cc9ce-default-rtdb.firebaseio.com/cart.json', 
-            {
-                method: 'POST', 
-                body: JSON.stringify(cart)
+        const sendCartData = async() => {
+            const response = await fetch(
+                'https://react-book-cart-cc9ce-default-rtdb.firebaseio.com/cart.json', 
+                {
+                    method: 'POST', 
+                    body: JSON.stringify(cart)
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed');
             }
-        );
+
+            const responseData = await response.json();
+        };
     }, [cart]);
 
     return (
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,10 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartIsVisible: false },
+    initialState: { cartIsVisible: false, notification: null },
     reducers: {
         toggle(state) {
             state.cartIsVisible = !state.cartIsVisible;
+        },
+        showNotification(state, action) {
+            state.notification = {
+                status: state.action.status,    // status like pending, error, success...
+                title: state.action.title,
+                message: state.action.message,
+            };
         }
     }
 });
