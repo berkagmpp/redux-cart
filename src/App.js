06963ca114aacb91097bd639a67649bb0ae7d037
berkagmpp <berkagmpp@gmail.com>
2022-05-23 15:10:00 +1200

    import Notification and add codes to show noti using useSelector() / make return noti in itial in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notificatioin';
 import { uiActions } from './store/ui-slice';
 
+let isInitial = true;
+
 function App() {
+    const dispatch = useDispatch();
     const showCart = useSelector(state => state.ui.cartIsVisible);
     const cart = useSelector(state => state.cart);
-    const dispatch = useDispatch();
-    
+    const notification = useSelector(state => state.ui.notification);
+
     useEffect(() => {
         const sendCartData = async() => {
             dispatch(uiActions.showNotification({
@@ -40,6 +44,11 @@ function App() {
             }));
         };
 
+        if (isInitial) {
+            isInitial = false;
+            return;
+        }
+
         sendCartData().catch(error =>{
             dispatch(uiActions.showNotification({
                 status: 'error',
@@ -51,10 +60,15 @@ function App() {
     }, [cart, dispatch]);
 
     return (
-        <Layout>
-            {showCart && <Cart />}
-            <Products />
-        </Layout>
+        <Fragment>
+            {notification && <Notification status={notification.status} 
+                                           title={notification.title} 
+                                           message={notification.message} />}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
     );
 }
 
