5b0a8a4e3d6e62957035e268e03b267b18fb3a55
berkagmpp <berkagmpp@gmail.com>
2022-05-24 21:54:20 +1200

    revise side-effect logic from inside the App component to be inside the action creators as a thunk, cart-slice.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notificatioin';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -16,47 +16,53 @@ function App() {
     const notification = useSelector(state => state.ui.notification);
 
     useEffect(() => {
-        const sendCartData = async() => {
-            dispatch(uiActions.showNotification({
-                status: 'pending',
-                title: 'sending',
-                message:  'Sending cart data!'
-            }));
-
-            const response = await fetch(
-                'https://react-book-cart-cc9ce-default-rtdb.firebaseio.com/cart.json', 
-                {
-                    method: 'POST', 
-                    body: JSON.stringify(cart)
-                }
-            );
-
-            if (!response.ok) {
-                throw new Error('Sending cart data failed');
-            }
-
-            // const responseData = await response.json();  // dose not care in this case
-
-            dispatch(uiActions.showNotification({
-                status: 'success',
-                title: 'success',
-                message: 'Sent cart data successfully!'
-            }));
-        };
-
         if (isInitial) {
             isInitial = false;
             return;
         }
 
-        sendCartData().catch(error =>{
-            dispatch(uiActions.showNotification({
-                status: 'error',
-                title: 'error',
-                message: 'Sending cart data failed'
-            }));
-        });
+        dispatch(sendCartData(cart));   // side effect logic inside the action creator from cart-slice for leaner App component
 
+        // const sendCartData = async() => {   // side effect logic inside the App component
+        //     dispatch(uiActions.showNotification({
+        //         status: 'pending',
+        //         title: 'sending',
+        //         message:  'Sending cart data!'
+        //     }));
+
+        //     const response = await fetch(
+        //         'https://react-book-cart-cc9ce-default-rtdb.firebaseio.com/cart.json', 
+        //         {
+        //             method: 'POST', 
+        //             body: JSON.stringify(cart)
+        //         }
+        //     );
+
+        //     if (!response.ok) {
+        //         throw new Error('Sending cart data failed');
+        //     }
+
+        //     // const responseData = await response.json();  // dose not care in this case
+
+        //     dispatch(uiActions.showNotification({
+        //         status: 'success',
+        //         title: 'success',
+        //         message: 'Sent cart data successfully!'
+        //     }));
+        // };
+
+        // if (isInitial) {
+        //     isInitial = false;
+        //     return;
+        // }
+
+        // sendCartData().catch(error =>{
+        //     dispatch(uiActions.showNotification({
+        //         status: 'error',
+        //         title: 'error',
+        //         message: 'Sending cart data failed'
+        //     }));
+        // });
     }, [cart, dispatch]);
 
     return (
